853ea895538ead4b13d5033a686899275acb615e
IrisH <51488916+tye2002@users.noreply.github.com>
2021-12-02 00:05:45 +0700

    Add edit text button
    Bold, Italic, Underline

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,23 @@
+/*==================== STICKY BAR ====================*/
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("edit_button");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
 /*==================== SHOW MENU ====================*/
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
